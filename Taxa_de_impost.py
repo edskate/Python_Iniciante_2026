46af9bf4fb06f666dbb4711c89356eb8fd705765
Edy_97_2024 <edskateicevida@gmail.com>
2026-01-17 13:41:30 -0300

    Faturamento de modelo

---
+++ Taxa_de_impost.py
@@ -0,0 +1,24 @@
+faturamento = 1200 # Exemplo de valor de faturamento
+custo = 700
+
+novas_vendas = 300
+faturamento = faturamento + novas_vendas
+
+taxa_imposto = 0.1 # numero decimal para 10% -> float
+mensagem = "O faturamento foi de "
+teve_lucro = False # booleano -> bool
+
+
+impost = taxa_imposto * faturamento
+print("Imposto a ser pago:", impost)
+print("Faturamento:", faturamento)
+print("Custo:", custo)
+print("lucro", faturamento - custo - impost)
+print(Faturamento, faturamento)
+
+
+
+
+
+
+ 
