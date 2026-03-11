2d1741b124adba2218032bfe70ef9fe2b324477b
Edy_97_2024 <edskateicevida@gmail.com>
2026-03-11 00:21:01 -0300

    0.1.py
    Python

---
+++ 01.py
@@ -0,0 +1,10 @@
+nome = "Maria"       
+idade = 25          
+altura = 1.65        
+estudante = True     
+
+idade = 25
+_contador = 0
+nome_completo = "João Silva"
+Usuario1 = "Maria"
+CONSTANTE = 3.14
